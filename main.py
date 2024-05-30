81961235cd4bfc191d85b4705e94845534fdef44
van-geaux <github@alredho.com>
2024-05-30 11:21:47 +0700

    fixed the binary

--- main.py
+++ main.py
@@ -1,79 +1,78 @@
-import os
-import subprocess
-import sys
-
 from src.backend import db_check, random_pos_check
 from src.input import get_scraper_data
 from src.printer import data_print
 from src.scraper import map_scraper, map_scraper_with_scrolls, map_scraper_with_scrolls_deep
 from src.utilities import clean_table_name
 
-def check_and_install_env():
-    if not os.path.exists('env'):
-        print('Virtual environment tidak ditemukan. Membuat env...')
-        subprocess.check_call([sys.executable, '-m', 'venv', 'env'])
-        
-        print('Menginstall packages...')
-        subprocess.check_call([os.path.join('env', 'Scripts', 'pip'), 'install', '-r', 'requirements.txt'])
-    else:
-        print('Virtual environment ditemukan.')
+import os
+import psutil
+import sys
+import signal
 
-def activate_env():
-    env_path = os.path.join('env', 'Scripts')
-    if sys.platform == 'win32':
-        env_python = os.path.join(env_path, 'python.exe')
-    else:
-        env_python = os.path.join(env_path, 'python')
+def terminate_subprocesses():
+    current_process = psutil.Process()
+    for child in current_process.children(recursive=True):
+        child.terminate()
+    _, still_alive = psutil.wait_procs(current_process.children(recursive=True), timeout=5)
+    for child in still_alive:
+        child.kill()
 
-    os.environ['VIRTUAL_ENV'] = os.path.abspath('env')
-    os.environ['PATH'] = env_path + os.pathsep + os.environ['PATH']
-    sys.executable = env_python
-    sys.path = [env_path] + sys.path
+def signal_handler(sig, frame):
+    print("\nSignal received, terminating subprocesses...")
+    terminate_subprocesses()
+    sys.exit(0)
 
 def main():
-    check_and_install_env()
-    activate_env()
-    print('\n')
+    try:
+        signal.signal(signal.SIGINT, signal_handler)
+        signal.signal(signal.SIGTERM, signal_handler)
 
-    print("Menjalankan script. Tekan Ctrl+C untuk membatalkan.")
-    print('\n')
+        print("Menjalankan script. Tekan Ctrl+C untuk membatalkan.")
+        print('\n')
 
-    start_options = int(input('''1. Map scraping
+        start_options = int(input('''1. Map scraping
 2. Simpan data hasil scraping
                           
 Pilih kegiatan (ketik angka saja): '''))
-    
-    if start_options == 1:
-        database, proxy, jenis, propinsi, kota, kecamatan, kelurahan, scraper_input = get_scraper_data()
+        
+        if start_options == 1:
+            database, proxy, jenis, propinsi, kota, kecamatan, kelurahan, scraper_input = get_scraper_data()
 
-        print('\n')
-        filter_wilayah = {
-            'PROPINSI': propinsi.upper(),
-            'KOTA': kota.upper(),
-            'KECAMATAN': kecamatan.upper(),
-            'KELURAHAN': kelurahan.upper()
-        }
+            print('\n')
+            filter_wilayah = {
+                'PROPINSI': propinsi.upper(),
+                'KOTA': kota.upper(),
+                'KECAMATAN': kecamatan.upper(),
+                'KELURAHAN': kelurahan.upper()
+            }
 
-        db_check(database, clean_table_name(jenis, filter_wilayah))
-        random_pos_check(database)
+            db_check(database, clean_table_name(jenis, filter_wilayah))
+            random_pos_check(database)
 
-        if scraper_input == 1:
-            map_scraper(database, jenis, filter_wilayah, proxy)
-        elif scraper_input == 2:
-            map_scraper_with_scrolls(database, jenis, filter_wilayah, proxy)
-        elif scraper_input == 3:
-            map_scraper_with_scrolls_deep(database, jenis, filter_wilayah, proxy)
+            if scraper_input == 1:
+                map_scraper(database, jenis, filter_wilayah, proxy)
+            elif scraper_input == 2:
+                map_scraper_with_scrolls(database, jenis, filter_wilayah, proxy)
+            elif scraper_input == 3:
+                map_scraper_with_scrolls_deep(database, jenis, filter_wilayah, proxy)
+            else:
+                print('Pilihan invalid')
+
+        elif start_options == 2:
+            data_print()
+
         else:
             print('Pilihan invalid')
 
-    elif start_options == 2:
-        data_print()
+        print('\n')
+        input('Tekan Enter untuk keluar...: ')
 
-    else:
-        print('Pilihan invalid')
-
-    print('\n')
-    input('Tekan Enter untuk keluar...')
+    except KeyboardInterrupt:
+        print('\nScript dihentikan manual.')
+        input('Tekan Enter untuk keluar...: ')
+    finally:
+        terminate_subprocesses()
+        sys.exit()
 
 if __name__ == "__main__":    
     main()
